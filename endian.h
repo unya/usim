460c1dabb07bf3066f449d4f8db93d87b07d8c47
Alfred M. Szmidt <ams@gnu.org>
2017-08-14 08:46:59 +0000

    endian.h: Cleanup.

--- endian.h
+++ endian.h
@@ -2,20 +2,20 @@
 #define _USIM_ENDIAN_H_
 
 #if __BYTE_ORDER == __LITTLE_ENDIAN
-#define __LITTLE_ENDIAN__  1
+#define __LITTLE_ENDIAN__ 1
 #endif
 
 #ifdef OSX_X
 #include <CoreFoundation/CFByteOrder.h>
 #define SWAP_SHORT(x) CFSwapInt16LittleToHost(x)
 #define SWAP_LONG(x) CFSwapInt32LittleToHost(x)
-#else /* hand coded versions */
+#else
 #define SWAP_SHORT(x) ( (((x) & 0xff00) >> 8) | (((x) & 0x00ff) << 8) )
 #define SWAP_LONG(x) ( (((x) & 0xff000000) >> 24) |	\
 		       (((x) & 0x00ff0000) >> 8) |	\
 		       (((x) & 0x0000ff00) << 8) |	\
 		       (((x) & 0x000000ff) << 24) )
-#endif /* def OSX */
+#endif
 
 #ifdef __BIG_ENDIAN__
 #define LE_TO_SHORT(s) (SWAP_SHORT(s))
