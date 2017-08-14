d36399ea99bd9acf6034fe1abfcb26df8b044536
Alfred M. Szmidt <ams@gnu.org>
2017-08-14 19:39:17 +0000

    x11.c: Cleanup.

--- x11.c
+++ x11.c
@@ -11,10 +11,8 @@
 extern int run_ucode_flag;
 extern void iob_key_event(int code, int extra);
 extern void iob_mouse_event(int x, int y, int dx, int dy, int buttons);
-
 static unsigned int video_width = 768;
-static unsigned int video_height = 897 /*1024*/;
-
+static unsigned int video_height = 897;
 unsigned int tv_bitmap[(768 * 1024)];
 
 typedef struct DisplayState {
@@ -31,7 +29,6 @@ static int bitmap_order;
 static int color_depth;
 static Visual *visual = NULL;
 static GC gc;
-
 static XImage *ximage;
 
 #define USIM_EVENT_MASK ExposureMask | ButtonPressMask | ButtonReleaseMask | PointerMotionMask | KeyPressMask | KeyReleaseMask
@@ -40,42 +37,40 @@ static XImage *ximage;
 #define MOUSE_EVENT_MBUTTON 2
 #define MOUSE_EVENT_RBUTTON 4
 
-#define X_SHIFT            1
-#define X_CAPS             2
-#define X_CTRL             4
-#define X_ALT              8
-#define X_META            16
+#define X_SHIFT 1
+#define X_CAPS 2
+#define X_CTRL 4
+#define X_ALT 8
+#define X_META 16
 
 unsigned long Black, White;
 
 static int old_run_state;
+
 static XComposeStatus status;
 
-static void x11_process_key(XEvent *e, int updown)
+static void
+x11_process_key(XEvent * e, int updown)
 {
 	KeySym keysym;
 	unsigned char buffer[5];
 	int extra;
 
 	extra = 0;
+
 	if (e->xkey.state & X_META)
 		extra |= 3 << 12;
-
 	if (e->xkey.state & X_ALT)
 		extra |= 3 << 12;
-
 	if (e->xkey.state & X_SHIFT)
 		extra |= 3 << 6;
-
 	if (e->xkey.state & X_CAPS)
 		extra ^= 3 << 6;
-
 	if (e->xkey.state & X_CTRL)
 		extra |= 3 << 10;
 
 	if (updown) {
-		XLookupString(&e->xkey, (char *) buffer, 5, &keysym,
-				    &status);
+		XLookupString(&e->xkey, (char *) buffer, 5, &keysym, &status);
 		iob_key_event(keysym, extra);
 	}
 }
@@ -89,37 +84,28 @@ display_poll(void)
 	send_accumulated_updates();
 
 	while (XCheckWindowEvent(display, window, USIM_EVENT_MASK, &e)) {
-
 		switch (e.type) {
 		case Expose:
-			XPutImage(display, window, gc, ximage, 0, 0, 0, 0,
-				  video_width, video_height);
+			XPutImage(display, window, gc, ximage, 0, 0, 0, 0, video_width, video_height);
 			XFlush(display);
 			break;
-
 		case KeyPress:
 			x11_process_key(&e, 1);
 			break;
-
 		case KeyRelease:
 			x11_process_key(&e, 0);
 			break;
-
 		case MotionNotify:
 		case ButtonPress:
 		case ButtonRelease:
-			iob_mouse_event(e.xbutton.x, e.xbutton.y, 0, 0,
-					    e.xbutton.button);
+			iob_mouse_event(e.xbutton.x, e.xbutton.y, 0, 0, e.xbutton.button);
 			break;
-
 		default:
 			break;
 		}
 	}
-
 	if (old_run_state != run_ucode_flag) {
 		old_run_state = run_ucode_flag;
-		/*update_caption();*/
 	}
 }
 
@@ -132,20 +118,17 @@ video_read(int offset, unsigned int *pv)
 
 		offset *= 32;
 
-		if (offset > video_width*video_height) {
-			printf("video: video_read past end; "
-				      "offset %o\n", offset);
+		if (offset > video_width * video_height) {
+			printf("video: video_read past end; " "offset %o\n", offset);
 			*pv = 0;
 			return;
 		}
 
 		bits = 0;
-		for (i = 0; i < 32; i++)
-		{
+		for (i = 0; i < 32; i++) {
 			if (tv_bitmap[offset + i] == Black)
 				bits |= 1 << i;
 		}
-
 		*pv = bits;
 	}
 }
@@ -155,10 +138,14 @@ int u_minh = 0x7fffffff, u_maxh, u_minv = 0x7fffffff, u_maxv;
 void
 accumulate_update(int h, int v, int hs, int vs)
 {
-	if (h < u_minh) u_minh = h;
-	if (h+hs > u_maxh) u_maxh = h+hs;
-	if (v < u_minv) u_minv = v;
-	if (v+vs > u_maxv) u_maxv = v+vs;
+	if (h < u_minh)
+		u_minh = h;
+	if (h + hs > u_maxh)
+		u_maxh = h + hs;
+	if (v < u_minv)
+		u_minv = v;
+	if (v + vs > u_maxv)
+		u_maxv = v + vs;
 }
 
 void
@@ -168,8 +155,7 @@ send_accumulated_updates(void)
 
 	hs = u_maxh - u_minh;
 	vs = u_maxv - u_minv;
-	if (u_minh != 0x7fffffff && u_minv != 0x7fffffff && u_maxh && u_maxv)
-	{
+	if (u_minh != 0x7fffffff && u_minv != 0x7fffffff && u_maxh && u_maxv) {
 		XPutImage(display, window, gc, ximage, u_minh, u_minv, u_minh, u_minv, hs, vs);
 		XFlush(display);
 	}
@@ -191,10 +177,8 @@ video_write(int offset, unsigned int bits)
 		v = offset / video_width;
 		h = offset % video_width;
 
-		for (i = 0; i < 32; i++)
-		{
-			tv_bitmap[offset + i] =
-				(bits & 1) ? Black : White;
+		for (i = 0; i < 32; i++) {
+			tv_bitmap[offset + i] = (bits & 1) ? Black : White;
 			bits >>= 1;
 		}
 
@@ -218,14 +202,11 @@ display_init(void)
 	XTextProperty iconName;
 	XTextProperty *pIconName = &iconName;
 	XWMHints *wm_hints;
-
-	char *window_name = (char *)"CADR Emulator";
-	char *icon_name = (char *)"CADR";
+	char *window_name = (char *) "CADR Emulator";
+	char *icon_name = (char *) "CADR";
 
 	displayname = getenv("DISPLAY");
-
 	display = XOpenDisplay(displayname);
-
 	if (display == NULL) {
 		fprintf(stderr, "usim: failed to open display.\n");
 		exit(-1);
@@ -237,15 +218,10 @@ display_init(void)
 
 	Black = BlackPixel(display, xscreen);
 	White = WhitePixel(display, xscreen);
+
 	root = RootWindow(display, xscreen);
-
-	attr.event_mask = ExposureMask | KeyPressMask | KeyReleaseMask |
-		ButtonPressMask | ButtonReleaseMask | PointerMotionMask;
-
-	window = XCreateWindow(display, root, 0, 0, video_width, video_height,
-			       0, color_depth, InputOutput, visual,
-			       CWBorderPixel |  CWEventMask, &attr);
-
+	attr.event_mask = ExposureMask | KeyPressMask | KeyReleaseMask | ButtonPressMask | ButtonReleaseMask | PointerMotionMask;
+	window = XCreateWindow(display, root, 0, 0, video_width, video_height, 0, color_depth, InputOutput, visual, CWBorderPixel | CWEventMask, &attr);
 	if (window == None) {
 		fprintf(stderr, "usim: failed to open window.\n");
 		exit(-1);
@@ -260,11 +236,7 @@ display_init(void)
 	}
 
 	if ((size_hints = XAllocSizeHints()) != NULL) {
-
-		/*
-		 * The window will not be resizable:
-		 */
-
+		// The window will not be resizable.
 		size_hints->flags = PMinSize | PMaxSize;
 		size_hints->min_width = size_hints->max_width = video_width;
 		size_hints->min_height = size_hints->max_height = video_height;
@@ -276,34 +248,25 @@ display_init(void)
 		wm_hints->flags = StateHint | InputHint;
 	}
 
-	XSetWMProperties(display, window, pWindowName, pIconName, NULL, 0,
-			 size_hints, wm_hints, NULL);
-
+	XSetWMProperties(display, window, pWindowName, pIconName, NULL, 0, size_hints, wm_hints, NULL);
 	XMapWindow(display, window);
 
 	gc = XCreateGC(display, window, 0, &gcvalues);
 
-	/*
-	 * Fill window with the specified background color:
-	 */
-
+	// Fill window with the specified background color.
 	bg_pixel = 0;
 	XSetForeground(display, gc, bg_pixel);
 	XFillRectangle(display, window, gc, 0, 0, video_width, video_height);
 
-	/*
-	 * Wait for first Expose event to do any drawing, then flush:
-	 */
-
+	// Wait for first Expose event to do any drawing, then flush.
 	do {
 		XNextEvent(display, &e);
 	}
 	while (e.type != Expose || e.xexpose.count);
 
 	XFlush(display);
-
-	ximage = XCreateImage(display, visual, (unsigned)color_depth, ZPixmap, 0,
-			      (char *) tv_bitmap, video_width, video_height, 32, 0);
+	ximage = XCreateImage(display, visual, (unsigned) color_depth, ZPixmap, 0, (char *) tv_bitmap, video_width, video_height, 32, 0);
 	ximage->byte_order = LSBFirst;
+
 	return 0;
 }
