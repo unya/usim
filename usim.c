583d58430296eecf951677b029635cc0251e03c4
Alfred M. Szmidt <ams@gnu.org>
2017-08-14 20:32:24 +0000

    usim.c: Cleanup.

--- usim.c
+++ usim.c
@@ -6,13 +6,11 @@
 #include <signal.h>
 #include <string.h>
 #include <limits.h>
-
 #include <unistd.h>
 #include <sys/time.h>
 
 #include "ucode.h"
 #include "config.h"
-
 #include "Files.h"
 
 int show_video_flag;
@@ -21,7 +19,6 @@ int alt_prom_flag;
 int dump_state_flag;
 int save_state_flag;
 
-/* */
 extern int display_init(void);
 extern void display_poll(void);
 extern int disk_init(const char *filename);
@@ -32,13 +29,9 @@ extern int chaos_init(void);
 extern void iob_warm_boot_key(void);
 extern void run(void);
 extern int dcanon(char *cp, int blankok);
-
 struct timeval tv1;
 
-/*
- * simple wall clock timing to get a notion of the basic cycle time
- */
-
+// Simple wall clock timing to get a notion of the basic cycle time.
 void
 timing_start()
 {
@@ -56,20 +49,15 @@ timing_stop()
 	if (tv2.tv_usec < tv1.tv_usec) {
 		tv2.tv_sec--;
 		td.tv_usec = (tv2.tv_usec + 1000 * 1000) - tv1.tv_usec;
-	}
-	else
+	} else
 		td.tv_usec = tv2.tv_usec - tv1.tv_usec;
-
 	td.tv_sec = tv2.tv_sec - tv1.tv_sec;
 
-	t = (double)td.tv_sec + ((double)td.tv_usec / (1000.0 * 1000.0));
-
+	t = (double) td.tv_sec + ((double) td.tv_usec / (1000.0 * 1000.0));
 	cps = cycles / t;
 
 	printf("\ncycle timing:\n");
-
-	printf("%lu cycles in %g seconds, %10.8g cycles/second\n",
-	       cycles, t, cps);
+	printf("%lu cycles in %g seconds, %10.8g cycles/second\n", cycles, t, cps);
 	printf("%.0f ns/cycle\n", (t / cycles) * 1000.0 * 1000.0 * 1000.0);
 }
 
@@ -84,6 +72,7 @@ sighup_handler(int arg)
 {
 	char *b = "MPY";
 	extern int trace_late_set;
+
 	breakpoint_set_mcr(b);
 	printf("set breakpoint in %s\n", b);
 	trace_late_set = 1;
@@ -102,40 +91,38 @@ signal_shutdown(void)
 	fflush(stdout);
 }
 
-
 void
 usage(void)
 {
 	fprintf(stderr, "usage:\n");
-	fprintf(stderr, "-a		use alternate prom file\n");
-	fprintf(stderr, "-b <sym-name>	set breakpoint in microcode\n");
-	fprintf(stderr, "-B <number>	start tracing after # microcode cycles\n");
-	fprintf(stderr, "-c <number>	set max # of microcode cycles to run\n");
-	fprintf(stderr, "-C <number>	set max # of traced microcode cycles to run\n");
-	fprintf(stderr, "-d 		dump state\n");
-	fprintf(stderr, "-i <file>	set disk image\n");
-	fprintf(stderr, "-l <sym-name>	start tracing at symbol\n");
-	fprintf(stderr, "-n		run with no SDL video window\n");
-	fprintf(stderr, "-m 		mouse sync\n");
-	fprintf(stderr, "-p <sym-name>	set breakpoint in prom\n");
-	fprintf(stderr, "-q <number>	break after hitting breakpoint n times\n");
-	fprintf(stderr, "-r		map /tree to ../sys\n");
-	fprintf(stderr, "-S 		save state\n");
-	fprintf(stderr, "-t		turn on microcode tracing\n");
-	fprintf(stderr, "-T<flags>	turn on tracing\n");
-	fprintf(stderr, "   a - enable tracing after # cycles (see -B flag)\n");
-	fprintf(stderr, "   d - disk\n");
-	fprintf(stderr, "   i - interrupts\n");
-	fprintf(stderr, "   o - i/o\n");
-	fprintf(stderr, "   p - prom\n");
-	fprintf(stderr, "   c - microcode\n");
-	fprintf(stderr, "   m - microcode labels\n");
-	fprintf(stderr, "   n - network\n");
-	fprintf(stderr, "   l - lod labels\n");
-	fprintf(stderr, "   v - virtual memory\n");
-	fprintf(stderr, "-s		halt after prom runs\n");
-	fprintf(stderr, "-w		warm boot\n");
-
+	fprintf(stderr, "-a use alternate prom file\n");
+	fprintf(stderr, "-b <sym-name> set breakpoint in microcode\n");
+	fprintf(stderr, "-B <number> start tracing after # microcode cycles\n");
+	fprintf(stderr, "-c <number> set max # of microcode cycles to run\n");
+	fprintf(stderr, "-C <number> set max # of traced microcode cycles to run\n");
+	fprintf(stderr, "-d dump state\n");
+	fprintf(stderr, "-i <file> set disk image\n");
+	fprintf(stderr, "-l <sym-name> start tracing at symbol\n");
+	fprintf(stderr, "-n run with no SDL video window\n");
+	fprintf(stderr, "-m mouse sync\n");
+	fprintf(stderr, "-p <sym-name> set breakpoint in prom\n");
+	fprintf(stderr, "-q <number> break after hitting breakpoint n times\n");
+	fprintf(stderr, "-r map /tree to ../sys\n");
+	fprintf(stderr, "-S save state\n");
+	fprintf(stderr, "-t turn on microcode tracing\n");
+	fprintf(stderr, "-T<flags> turn on tracing\n");
+	fprintf(stderr, " a - enable tracing after # cycles (see -B flag)\n");
+	fprintf(stderr, " d - disk\n");
+	fprintf(stderr, " i - interrupts\n");
+	fprintf(stderr, " o - i/o\n");
+	fprintf(stderr, " p - prom\n");
+	fprintf(stderr, " c - microcode\n");
+	fprintf(stderr, " m - microcode labels\n");
+	fprintf(stderr, " n - network\n");
+	fprintf(stderr, " l - lod labels\n");
+	fprintf(stderr, " v - virtual memory\n");
+	fprintf(stderr, "-s halt after prom runs\n");
+	fprintf(stderr, "-w warm boot\n");
 	exit(1);
 }
 
@@ -195,7 +182,8 @@ main(int argc, char *argv[])
 		{
 			char *p = "../sys";
 			char newpath[PATH_MAX];
-			realpath (p, newpath);
+
+			realpath(p, newpath);
 			dcanon(newpath, 0);
 			settreeroot(newpath);
 		}
@@ -208,16 +196,36 @@ main(int argc, char *argv[])
 			break;
 		case 'T':
 			switch (optarg[0]) {
-			case 'a': trace_after_flag = 1; break;
-			case 'd': trace_disk_flag = 1; break;
-			case 'i': trace_int_flag = 1; break;
-			case 'o': trace_io_flag = 1; break;
-			case 'p': trace_prom_flag = 1; break;
-			case 'c': trace_mcr_flag = 1; break;
-			case 'm': trace_mcr_labels_flag = 1; break;
-			case 'n': trace_net_flag = 1; break;
-			case 'l': trace_lod_labels_flag = 1; break;
-			case 'v': trace_vm_flag = 1; break;
+			case 'a':
+				trace_after_flag = 1;
+				break;
+			case 'd':
+				trace_disk_flag = 1;
+				break;
+			case 'i':
+				trace_int_flag = 1;
+				break;
+			case 'o':
+				trace_io_flag = 1;
+				break;
+			case 'p':
+				trace_prom_flag = 1;
+				break;
+			case 'c':
+				trace_mcr_flag = 1;
+				break;
+			case 'm':
+				trace_mcr_labels_flag = 1;
+				break;
+			case 'n':
+				trace_net_flag = 1;
+				break;
+			case 'l':
+				trace_lod_labels_flag = 1;
+				break;
+			case 'v':
+				trace_vm_flag = 1;
+				break;
 			}
 			break;
 		case 's':
@@ -236,10 +244,9 @@ main(int argc, char *argv[])
 		display_poll();
 	}
 
-	disk_init( config_get_disk_filename() );
+	disk_init(config_get_disk_filename());
 
 	read_prom_files();
-
 	read_sym_files();
 
 	iob_init();
