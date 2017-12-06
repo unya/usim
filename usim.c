b12187337d3fba66b077f33ce82647cb667e6388
Alfred M. Szmidt <ams@gnu.org>
2017-12-06 18:18:28 +0000

    usim.c (main): Remove bogus switch.

--- usim.c
+++ usim.c
@@ -57,7 +57,7 @@ main(int argc, char *argv[])
 
 	printf("CADR emulator v0.9\n");
 
-	while ((c = getopt(argc, argv, "i:rSswl")) != -1) {
+	while ((c = getopt(argc, argv, "i:Sswl")) != -1) {
 		switch (c) {
 		case 'i':
 			disk_filename = strdup(optarg);
